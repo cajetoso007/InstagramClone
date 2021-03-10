83c6a1bf09507164acf9a789bc1cc2ca3b323969
Manny <manuel.reyes0016@gmail.com>
2021-03-10 13:56:41 -0800

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Manny Reyes on 3/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
